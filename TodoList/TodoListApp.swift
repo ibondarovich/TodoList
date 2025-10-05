dc76fa3a96fb4f54a035913aa1405d22ea501025
Ilona Bondarovich <ilona.bondarovich@gmail.com>
2025-10-05 10:30:25 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by user on 05/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
